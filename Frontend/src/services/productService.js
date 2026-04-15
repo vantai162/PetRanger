844ddbbec96a5764adb4197c9293b716bce2c291
Hoàng Văn Tài <hoangvantai162@gmail.com>
2026-04-15 13:09:45 +0700

    Frontend: fix product bug

--- Frontend/src/services/productService.js
+++ Frontend/src/services/productService.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "${import.meta.env.VITE_API_BASE_URL}/api/products";
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api/products`;
+
 
 export async function getAllProducts() {
   const res = await fetch(`${API_BASE_URL}`);
